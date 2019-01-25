8531f81bbe47cb760ef6b58ec6391b1f10ab5e86
Varun Babbar <vb395@cam.ac.uk>
2019-01-25 12:40:35 +0000

    Modified geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -12,6 +12,36 @@ from . import datafetcher
 from .stationdata import build_station_list
 from .station import MonitoringStation
 from haversine import haversine
+from .utils import sorted_by_key
+import math
+
+# formula for distance between two geographic coordinates (in km)
+def haversine_formula(x1, y1, x2, y2):
+    # difference in x and y
+    d_X = (x2 - x1) * math.pi / 180.0
+    d_Y = (y2 - y1) * math.pi / 180.0
+
+    # converting x1 and x2 to radians
+    x1 = x1 * math.pi / 180.0
+    x2 = x2 * math.pi / 180.0
+
+    a= math.sin(d_X/2)**2 + math.cos(x1)*math.cos(x2)*(math.sin(d_Y/2)**2)
+    # radius,r of Earth (assuming that it is a perfect sphere)
+    r = 6371
+    distance = 2*r* math.asin(math.sqrt(a))
+    return distance
+
+#Task 1B
+def stations_by_distance(stations, p):
+    station_distance = []
+    for station in stations:
+        coord_p_distance = haversine_formula(station.coord[0], station.coord[1], p[0], p[1])
+        name_distance = station.name, station.town, coord_p_distance
+        station_distance.append(name_distance)
+    sorted_stations = sorted_by_key(station_distance, 2, reverse=False)
+
+
+    return sorted_stations
 
 # Task 1C
 def stations_within_radius(stations, centre, r):
@@ -42,61 +72,6 @@ def stations_by_river(stations):
                 station_list.append(j.name)
         river_dictionary[i.river] = station_list
     return river_dictionary
-
-from .utils import sorted_by_key
-from . import datafetcher
-from .station import MonitoringStation
-import math 
-  
-# formula for distance between two geographic coordinates (in km)   
-def haversine_formula(x1, y1, x2, y2):
-     # difference in x and y
-    d_X = (x2 - x1) * math.pi / 180.0
-    d_Y = (y2 - y1) * math.pi / 180.0
-
-    # converting x1 and x2 to radians
-    x1 = x1 * math.pi / 180.0
-    x2 = x2 * math.pi / 180.0
-
-    a= math.sin(d_X/2)**2 + math.cos(x1)*math.cos(x2)*(math.sin(d_Y/2)**2)
-    # radius,r of Earth (assuming that it is a perfect sphere)
-    r = 6371
-    distance = 2*r* math.asin(math.sqrt(a))
-    return distance
-
-#Task 1B
-def stations_by_distance(stations, p):
-    station_distance = []
-    for station in stations:
-        coord_p_distance = haversine_formula(station.coord[0], station.coord[1], p[0], p[1])
-        name_distance = station.name, station.town, coord_p_distance
-        station_distance.append(name_distance)
-    sorted_stations = sorted_by_key(station_distance, 2, reverse=False)
-
-
-    return sorted_stations
-
-#Task 1D(1)
-def rivers_with_station(stations):
-    rivers = []
-    for station in stations:
-        if station.name != None:
-            if station.river not in rivers:
-                rivers.append(station.river)            
-    return rivers
-
-#Task 1D(2)
-def stations_by_river(stations):
-    """Creates a dictionary of stations that are located on the same river"""
-    river_dictionary={}
-    for i in stations:
-        station_list=[]
-        for j in stations:
-            if i.river == j.river:
-                station_list.append(j.name)
-        river_dictionary[i.river]=station_list
-    return river_dictionary
-
 
 #Task 1E
 def rivers_by_station_number(stations, N):
