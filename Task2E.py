f1fb6fb9dbd9d3e170f2c56746611f9e7ebff2e9
Varun Babbar <vb395@cam.ac.uk>
2019-02-22 16:18:55 +0000

    Added Task2E.py

---
+++ Task2E.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta, date, time
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list
+from floodsystem.plot import plot_water_levels
+from floodsystem.flood import stations_level_over_threshold
+
+
+stations = build_station_list()
+a = stations_level_over_threshold(stations, 0.8)
+x = a[:5]
+m = []
+for i in range(0,5):
+    m.append(x[i][0])
+print(m)
+
+for i in stations:
+    for k in m:
+        if k == i.name:
+            dates, levels = fetch_measure_levels(i.measure_id, dt = timedelta(days = 10))
+            plot_water_levels(i, dates, levels)
