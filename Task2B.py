99c575243dd0070fa463b7b54c35afc558d665b4
Varun Babbar <vb395@cam.ac.uk>
2019-02-25 22:42:14 +0000

    .

--- Task2B.py
+++ Task2B.py
@@ -4,6 +4,6 @@ from floodsystem.stationdata import update_water_levels
 
 stations = build_station_list()
 update_water_levels(stations)
-tol = 1.4
+tol = 0.8
 print("Stations with a relative water level greater than tolerance:")
 print(stations_level_over_threshold(stations, tol))
