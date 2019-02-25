800a67ac0690883a8d381b62dd04fb901b9f8e3b
Varun Babbar <vb395@cam.ac.uk>
2019-02-25 01:35:14 +0000

    Added flood.py

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -7,11 +7,13 @@ def stations_level_over_threshold(stations ,tol):
     b = []
     stations = build_station_list()
     update_water_levels(stations)
+
     for i in stations:
         if i.relative_water_level(i.latest_level) is not None:
             if i.relative_water_level(i.latest_level) > tol:
                 b.append(i.relative_water_level(i.latest_level))
     b.sort(reverse = True)
+
     for j in b:
         for k in stations:
             if j == k.relative_water_level(k.latest_level):
